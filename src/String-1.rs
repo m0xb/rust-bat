a01c978bd786b744430e177d3ce013b44c26c0be
Alan Campbell <ahcampbellster@gmail.com>
2017-11-23 04:04:59 -0700

    Added third column from String-1 bats, String-1.rs now contains all bats + tests (-other tets) from String-1.

--- src/String-1.rs
+++ src/String-1.rs
@@ -188,6 +188,127 @@ fn withoutX(mut s: String) -> String {
     return s;
 }
 
+fn makeTags(tag: String, word: String) -> String {
+    let mut left_tag = String::from("<");
+    let mut right_tag = String::from("</");
+    let mut new_s = String::from("");
+    left_tag += &tag;
+    left_tag += ">";
+    new_s += &left_tag;
+    new_s += &word;
+    right_tag += &tag;
+    right_tag += ">";
+    new_s += &right_tag;
+    return new_s;
+}
+
+fn firstTwo(mut s: String) -> String {
+    if s.len() > 1 {
+        s = s[0..2].to_string();
+    }
+    return s;
+}
+
+fn comboString(mut a: String, mut b: String) -> String {
+    let mut new_s = String::from("");
+    if a.len() < b.len() {
+        b.push_str(&a.to_string());
+        a.push_str(&b.to_string());
+        return a;
+    }
+    a.push_str(&b.to_string());
+    b.push_str(&a.to_string());
+    return b;
+}
+
+fn right2(s: String) -> String {
+    let mut new_s = String::with_capacity(s.len());
+    if s.len() > 2 {
+        new_s.push_str(&s[(s.len()-2)..(s.len())]);
+        new_s.push_str(&s[0..(s.len() - 2)]);
+        return new_s;
+    }
+    return s;
+}
+
+fn middleTwo(mut s: String) -> String {
+    s = s[(s.len()/2 - 1)..(s.len()/2 + 1)].to_string();
+    return s;
+}
+
+fn twoChar(mut s: String, index: i32) -> String {
+    let ui = index.abs() as usize; 
+    if ui + 2 <= s.len() && index > 0 {
+        s = s[ui..(ui+2)].to_string();
+    } else {
+        s = s[0..2].to_string();
+    }
+    return s;
+}
+
+fn atFirst(mut s: String) -> String {
+    if s.len() > 1 {
+        s = s[0..2].to_string();
+    } else if s.len() == 1 {
+        s = s[0..1].to_string();
+        s.push('@');
+    } else {
+        s = "@@".to_string();
+    }
+    return s;
+}
+
+fn lastTwo(s: String) -> String {
+    let mut new_s = String::from("");
+    if s.len() > 1 {
+        new_s.push_str(&s[0..(s.len() - 2)]);
+        new_s.push(s.chars().nth(s.len() - 1).unwrap());
+        new_s.push(s.chars().nth(s.len() - 2).unwrap());
+        return new_s;
+    }
+    return s;
+}
+
+//I... probably shouldn't have done this with mutable input strings.
+fn minCat(mut a: String, mut b: String) -> String {
+    let a_len = a.len();
+    if a.len() == b.len() {
+        a += &b.to_string();
+        return a;
+    } else if a.len() > b.len() {
+        a = a[(a.len() - b.len())..a.len()].to_string();
+        a += &b.to_string();
+        return a;
+    } else {
+        a.push_str(&b[(b.len() - a_len)..b.len()].to_string());
+        return a;
+    }
+}
+
+fn deFront(mut s: String) -> String {
+    if s.len() > 0 && s.chars().nth(0).unwrap() != 'a' {
+        s.remove(0);
+        if s.len() > 0 && s.chars().nth(0).unwrap() != 'b' {
+            s.remove(0);
+        }
+    } else if s.len() > 1 && s.chars().nth(1).unwrap() != 'b' {
+        s.remove(1);
+    }
+    return s;
+}
+
+fn withoutX2(mut s: String) -> String {
+    if s.len() > 0 && s.chars().nth(0).unwrap() == 'x' {
+        s.remove(0);
+        if s.len() > 0 && s.chars().nth(0).unwrap() == 'x' {
+            s.remove(0);
+        }
+    } else if s.len() > 1 && s.chars().nth(1).unwrap() == 'x' {
+        s.remove(1);
+    }
+    return s;
+}
+
 fn main() {
     let helloName_tests = vec![
         "Bob",
@@ -529,7 +650,186 @@ fn main() {
     for i in withoutX_tests {
         println!("withoutX: {}", withoutX(i.to_string()));
     }
+    
+    println!("\n");
 
+    let makeTags_tests = vec![
+        ("i", "Yay"),
+        ("i", "Hello"),
+        ("cite", "Yay"),
+        ("address", "here"),
+        ("body", "Heart"),
+        ("i", "i"),
+        ("i", "")];
+    for i in makeTags_tests {
+        println!("makeTags: {}", makeTags(i.0.to_string(), i.1.to_string()));
+    }
 
+    println!("\n");
+
+    let firstTwo_tests = vec![
+        "Hello",
+        "abcdefg",
+        "ab",
+        "a",
+        "",
+        "Kitten",
+        "hi",
+        "hiya"];
+    for i in firstTwo_tests {
+        println!("firstTwo: {}", firstTwo(i.to_string()));
+    }
+    
+    println!("\n");
+
+    let comboString_tests = vec![
+        ("Hello", "hi"),
+        ("hi", "Hello"),
+        ("aaa", "b"),
+        ("b", "aaa"), 
+        ("", "bb"),
+        ("aaa", "1234"),
+        ("aaa", "bb"),
+        ("a", "bb"),
+        ("bb", "a"),
+        ("xyz", "ab")];
+    for i in comboString_tests {
+        println!("comboString: {}", comboString(i.0.to_string(), i.1.to_string()))
+    }
+
+    println!("\n");
+
+    let right2_tests = vec![
+        "Hello",
+        "java",
+        "Hi",
+        "code",
+        "cat",
+        "12345"];
+    for i in right2_tests {
+        println!("right2: {}", right2(i.to_string()));
+    }
+    
+    println!("\n");
+
+    let middleTwo_tests = vec![
+        "string",
+        "code",
+        "Practice",
+        "ab",
+        "0123456789"];
+    for i in middleTwo_tests {
+        println!("middleTwo: {}", middleTwo(i.to_string()));
+    }
+
+    println!("\n");
+
+    let twoChar_tests = vec![
+        ("java", 0),
+        ("java", 2),
+        ("java", 3),
+        ("java", 4),
+        ("java", -1),
+        ("Hello", 0),
+        ("Hello", 1),
+        ("Hello", 99),
+        ("Hello", 3),
+        ("Hello", 4),
+        ("Hello", 5),
+        ("Hello", -7),
+        ("Hello", 6),
+        ("Hello", -1),
+        ("yay", 0)];
+    for i in twoChar_tests {
+        println!("twoChar: {}", twoChar(i.0.to_string(), i.1));
+    }
+    
+    println!("\n");
+
+    let atFirst_tests = vec![
+        "hello",
+        "hi",
+        "h",
+        "",
+        "kitten",
+        "java",
+        "j"];
+    for i in atFirst_tests {
+        println!("atFirst: {}", atFirst(i.to_string()));
+    }
+    
+    println!("\n");
+
+    let lastTwo_tests = vec![
+        "coding",
+        "cat",
+        "ab",
+        "a",
+        ""];
+    for i in lastTwo_tests {
+        println!("lastTwo: {}", lastTwo(i.to_string()));
+    }
+    
+    println!("\n");
+
+    let minCat_tests = vec![
+        ("Hello", "hi"),
+        ("Hello", "java"),
+        ("java", "Hello"),
+        ("abc", "x"),
+        ("x", "abc"),
+        ("abc", ""),
+        //other tests
+        ("Hello", "Hello"),
+        ("", "")];
+    
+    for i in minCat_tests {
+        println!("minCat: {}", minCat(i.0.to_string(), i.1.to_string()));
+    }
+    
+    println!("\n");
+
+    let deFront_tests = vec![
+        "Hello",
+        "java",
+        "away",
+        "axy",
+        "abc",
+        "xby",
+        "ab",
+        "ax",
+        "axb", 
+        "aaa",
+        "xbc",
+        "bbb",
+        "bazz",
+        "ba",
+        "abxyz",
+        "hi",
+        "his",
+        "xz",
+        "zzz"];
+    for i in deFront_tests {
+        println!("deFront: {}", deFront(i.to_string()));
+    }
+    
+    println!("\n");
+
+    let withoutX2_tests = vec![
+        "xHi",
+        "Hxi",
+        "Hi",
+        "xxHi",
+        "Hix",
+        "xaxb",
+        "xx",
+        "x",
+        "",
+        "Hello",
+        "Hexllo",
+        "xHxllo"];
+    for i in withoutX2_tests {
+        println!("withoutX2: {}", withoutX2(i.to_string()));
+    }
 }
 
