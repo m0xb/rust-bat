34230a8e358d2707146225318badb1bb5c8f1e2a
WorryingWonton <ahcampbellster@gmail.com>
2018-01-12 17:38:47 -0700

    Modified the test cases in Functional-2.rs to bring them inline with the input format for the printbat!~macro.  Also added some regex to help convert existing test cases in other bats over to the format needed for the printbat! macro.

--- src/Functional-2.rs
+++ src/Functional-2.rs
@@ -1,3 +1,4 @@
+
 fn noNeg(list: Vec<i32>) -> Vec<i32> {
    return list.into_iter().filter(|&x| !x.is_negative()).collect()
 }
@@ -38,141 +39,116 @@ fn square56(mut list: Vec<i32>) -> Vec<i32> {
     return list.into_iter().filter(|x| x % 10 != 5 && x % 10 != 6).collect()
 }
 
-
+#[macro_use]
+mod bat_formatter;
 fn main() {
-    let noNeg_tests = vec![
-        (vec![1, -2], vec![1]),
-        (vec![-3, -3, 3, 3], vec![3, 3]),
-        (vec![-1, -1, -1], vec![]),
-        (vec![], vec![]),
-        (vec![0, 1, 2], vec![0, 1, 2]),
-        (vec![3, -10, 1, -1, 4, -400], vec![3, 1, 4]),
-        (vec![-1, 3, 1, -1, -10, -100, -111, 5], vec![3, 1, 5])];
-    for i in noNeg_tests {
-        println!("noNeg: {:?} -> {:?}", i.1, noNeg(i.0));
-    }
+    printbat!(noNeg,
+        vec![1, -2] => vec![1],
+        vec![-3, -3, 3, 3] => vec![3, 3],
+        vec![-1, -1, -1] => Vec::<i32>::new(),
+        Vec::<i32>::new() => Vec::<i32>::new(),
+        vec![0, 1, 2] => vec![0, 1, 2],
+        vec![3, -10, 1, -1, 4, -400] => vec![3, 1, 4],
+        vec![-1, 3, 1, -1, -10, -100, -111, 5] => vec![3, 1, 5]);
 
     println!();
 
-    let no9_tests = vec![
-        (vec![1, 2, 19], vec![1, 2]),
-        (vec![9, 19, 29, 3], vec![3]),
-        (vec![1, 2, 3], vec![1, 2, 3]),
-        (vec![45, 99, 90, 28, 13, 999, 0], vec![45, 90, 28, 13, 0]),
-        (vec![], vec![]),
-        (vec![9], vec![]),
-        (vec![0, 9, 0], vec![0, 0])];
-    for i in no9_tests {
-        println!("no9: {:?} -> {:?}", i.1, no9(i.0));
-    }
+    printbat!(no9,
+        vec![1, 2, 19] => vec![1, 2],
+        vec![9, 19, 29, 3] => vec![3],
+        vec![1, 2, 3] => vec![1, 2, 3],
+        vec![45, 99, 90, 28, 13, 999, 0] => vec![45, 90, 28, 13, 0],
+        Vec::<i32>::new() => Vec::<i32>::new(),
+        vec![9] => Vec::<i32>::new(),
+        vec![0, 9, 0] => vec![0, 0]);
 
     println!();
 
-    let noTeen_tests = vec![
-        (vec![12, 13, 19, 20], vec![12, 20]),
-        (vec![1, 14, 1], vec![1, 1]),
-        (vec![15], vec![]),
-        (vec![-15], vec![-15]),
-        (vec![], vec![]),
-        (vec![0, 1, 2, -3], vec![0, 1, 2, -3]),
-        (vec![15, 17, 19, 21, 19], vec![21]),
-        (vec![-16, 2, 15, 3, 16, 25], vec![-16, 2, 3, 25])];
-    for i in noTeen_tests {
-        println!("noTeen: {:?} -> {:?}", i.1, noTeen(i.0));
-    }
+    printbat!(noTeen,
+        vec![12, 13, 19, 20] => vec![12, 20],
+        vec![1, 14, 1] => vec![1, 1],
+        vec![15] => Vec::<i32>::new(),
+        vec![-15] => vec![-15],
+        Vec::<i32>::new() => Vec::<i32>::new(),
+        vec![0, 1, 2, -3] => vec![0, 1, 2, -3],
+        vec![15, 17, 19, 21, 19] => vec![21],
+        vec![-16, 2, 15, 3, 16, 25] => vec![-16, 2, 3, 25]);
 
     println!();
 
-    let noLong_tests = vec![
-        (vec!["this", "not", "too", "long"], vec!["not", "too"]),
-        (vec!["a", "bbb", "cccc"],vec!["a", "bbb"]),
-        (vec!["cccc", "cccc", "cccc"], vec![]),
-        (vec![], vec![]),
-        (vec![""], vec![""]),
-        (vec!["empty", "", "empty"], vec![""]),
-        (vec!["a"], vec!["a"]),
-        (vec!["aaaa", "bbb", "***", "333"], vec!["bbb", "***", "333"])];
-    for i in noLong_tests {
-        println!("noLong: {:?} -> {:?}", i.1, noLong(i.0));
-    }
+    printbat!(noLong,
+        vec!["this", "not", "too", "long"] => vec!["not", "too"],
+        vec!["a", "bbb", "cccc"] => vec!["a", "bbb"],
+        vec!["cccc", "cccc", "cccc"] => Vec::<&str>::new(),
+        Vec::<&str>::new() => Vec::<&str>::new(),
+        vec![""] => vec![""],
+        vec!["empty", "", "empty"] => vec![""],
+        vec!["a"] => vec!["a"],
+        vec!["aaaa", "bbb", "***", "333"] => vec!["bbb", "***", "333"]);
 
     println!();
 
-    let noZ_tests = vec![
-        (vec!["aaa", "bbb", "aza"], vec!["aaa", "bbb"]),
-        (vec!["hziz", "hzello", "hi"], vec!["hi"]),
-        (vec!["hello", "howz", "are", "youz"], vec!["hello", "are"]),
-        (vec![], vec![]),
-        (vec![""], vec![""]),
-        (vec!["x", "y", "z"], vec!["x", "y"])];
-    for i in noZ_tests {
-        println!("noZ: {:?} -> {:?}", i.1, noZ(i.0));
-    }
+    printbat!(noZ,
+        vec!["aaa", "bbb", "aza"] => vec!["aaa", "bbb"],
+        vec!["hziz", "hzello", "hi"] => vec!["hi"],
+        vec!["hello", "howz", "are", "youz"] => vec!["hello", "are"],
+        Vec::<&str>::new() => Vec::<&str>::new(),
+        vec![""] => vec![""],
+        vec!["x", "y", "z"] => vec!["x", "y"]);
 
     println!();
 
-    let no34_tests = vec![
-        (vec!["a", "bb", "ccc"], vec!["a", "bb"]),
-        (vec!["a", "bb", "ccc", "dddd"], vec!["a", "bb"]),
-        (vec!["ccc", "dddd", "apple"], vec!["apple"]),
-        (vec!["this", "not", "too", "long"], vec![]),
-        (vec!["a", "bbb", "cccc", "xx"], vec!["a", "xx"]),
-        (vec!["dddd", "ddd", "xxxxxxx"], vec!["xxxxxxx"]),
-        (vec![], vec![]),
-        (vec![""], vec![""]),
-        (vec!["empty", "", "empty"], vec!["empty", "", "empty"]),
-        (vec!["a"], vec!["a"]),
-        (vec!["aaaa", "bbb", "*****", "333"], vec!["*****"])];
-    for i in no34_tests {
-        println!("no34: {:?} -> {:?}", i.1, no34(i.0));
-    }
+    printbat!(no34,
+        vec!["a", "bb", "ccc"] => vec!["a", "bb"],
+        vec!["a", "bb", "ccc", "dddd"] => vec!["a", "bb"],
+        vec!["ccc", "dddd", "apple"] => vec!["apple"],
+        vec!["this", "not", "too", "long"] => Vec::<&str>::new(),
+        vec!["a", "bbb", "cccc", "xx"] => vec!["a", "xx"],
+        vec!["dddd", "ddd", "xxxxxxx"] => vec!["xxxxxxx"],
+        Vec::<&str>::new() => Vec::<&str>::new(),
+        vec![""] => vec![""],
+        vec!["empty", "", "empty"] => vec!["empty", "", "empty"],
+        vec!["a"] => vec!["a"],
+        vec!["aaaa", "bbb", "*****", "333"] => vec!["*****"]);
 
     println!();
 
-    let noYY_tests = vec![
-        (vec!["a", "b", "c"], vec!["ay", "by", "cy"]),
-        (vec!["a", "b", "cy"], vec!["ay", "by"]),
-        (vec!["xx", "ya", "zz"], vec!["xxy", "yay", "zzy"]),
-        (vec!["xx", "yay", "zz"],vec!["xxy", "zzy"]),
-        (vec!["yyx", "y", "zzz"], vec!["zzzy"]),
-        (vec!["hello", "there"], vec!["helloy", "therey"]),
-        (vec!["ya"], vec!["yay"]),
-        (vec![], vec![]),
-        (vec![""], vec!["y"]),
-        (vec!["xx", "yy", "zz"], vec!["xxy", "zzy"])];
-    for i in noYY_tests {
-        println!("noYY: {:?} -> {:?}", i.1, noYY(i.0));
-    }
+    printbat!(noYY,
+        vec!["a", "b", "c"] => vec!["ay", "by", "cy"],
+        vec!["a", "b", "cy"] => vec!["ay", "by"],
+        vec!["xx", "ya", "zz"] => vec!["xxy", "yay", "zzy"],
+        vec!["xx", "yay", "zz"] => vec!["xxy", "zzy"],
+        vec!["yyx", "y", "zzz"] => vec!["zzzy"],
+        vec!["hello", "there"] => vec!["helloy", "therey"],
+        vec!["ya"] => vec!["yay"],
+        Vec::<&str>::new() => Vec::<&str>::new(),
+        vec![""] => vec!["y"],
+        vec!["xx", "yy", "zz"] => vec!["xxy", "zzy"]);
 
     println!();
 
-    let two2_tests =  vec![
-        (vec![1, 2, 3], vec![4, 6]),
-        (vec![2, 6, 11], vec![4]),
-        (vec![0], vec![0]),
-        (vec![], vec![]),
-        (vec![1, 11, 111, 16], vec![]),
-        (vec![2, 3, 5, 7, 11], vec![4, 6, 10, 14]),
-        (vec![3, 1, 4, 1, 6, 99, 0], vec![6, 8, 198, 0])];
-    for i in two2_tests {
-        println!("two2: {:?} -> {:?}", i.1, two2(i.0));
-    }
+    printbat!(two2,
+        vec![1, 2, 3] => vec![4, 6],
+        vec![2, 6, 11] => vec![4],
+        vec![0] => vec![0],
+        Vec::<i32>::new() => Vec::<i32>::new(),
+        vec![1, 11, 111, 16] => Vec::<i32>::new(),
+        vec![2, 3, 5, 7, 11] => vec![4, 6, 10, 14],
+        vec![3, 1, 4, 1, 6, 99, 0] => vec![6, 8, 198, 0]);
 
     println!();
 
-    let square56_tests = vec![
-        (vec![3, 1, 4], vec![19, 11]),
-        (vec![1], vec![11]),
-        (vec![2], vec![14]),
-        (vec![3], vec![19]),
-        (vec![4], vec![]),
-        (vec![5], vec![]),
-        (vec![6], vec![]),
-        (vec![7], vec![59]),
-        (vec![1, 2, 3, 4, 5, 6, 7], vec![11, 14, 19, 59]),
-        (vec![3, -1, -4, 1, 4, 5, 9], vec![19, 11, 11, 91])];
-    for i in square56_tests {
-        println!("square56: {:?} -> {:?}", i.1, square56(i.0));
-    }
+    printbat!(square56,
+        vec![3, 1, 4] => vec![19, 11],
+        vec![1] => vec![11],
+        vec![2] => vec![14],
+        vec![3] => vec![19],
+        vec![4] => Vec::<i32>::new(),
+        vec![5] => Vec::<i32>::new(),
+        vec![6] => Vec::<i32>::new(),
+        vec![7] => vec![59],
+        vec![1, 2, 3, 4, 5, 6, 7] => vec![11, 14, 19, 59],
+        vec![3, -1, -4, 1, 4, 5, 9] => vec![19, 11, 11, 91]);
+
 
 }
