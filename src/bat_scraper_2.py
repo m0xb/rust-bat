bd23843a11b2c52c354792f238d596129f7be1ea
Alan Campbell <ahcampbellster@gmail.com>
2018-01-13 00:19:34 -0700

    Early functional version of bat_scraper_2, can determine the return type of a bat from its html.

--- src/bat_scraper_2.py
+++ src/bat_scraper_2.py
@@ -1,30 +1,48 @@
 import unittest
+import re
 import requests
 from bs4 import BeautifulSoup
-def get_type(pid):
-    search_list = ['public ', 'private ', 'protected ']
-    ignore_list = ['static ', 'final ']
-    response = requests.post('http://codingbat.com/prob/', data=('id': pid))
+def get_bat(pid):
+    response = requests.post(f'http://codingbat.com/prob/{pid}')
+    return BeautifulSoup(response.text, 'html.parser').select('#ace_div')[0].text
 
-    return ''
+def get_type(text):
+    my_reg = re.search('(public|private|protected) ((static|final) )?([0-9a-zA-Z<>\[\]]*) ', text)
+    return my_reg.group(4)
+
+
+
+# bat = get_bat('p159531')
+# print(bat)
+# print(get_type(bat))
 
 class Testget_type(unittest.TestCase):
     def test_bool(self):
-        self.assertEqual('', get_type())
-    def test_int_array(self):
-        pass
+        self.assertEqual('boolean', get_type('public boolean lucky13(int[] nums) {}'))
+    def test_list_int(self):
+        self.assertEqual('List<Integer>', get_type('public List<Integer> no9(List<Integer> nums) {}'))
+    def test_list_string(self):
+        self.assertEqual('List<String>', get_type('public List<String> noYY(List<String> strings) {}'))
     def test_string_array(self):
-        pass
+        self.assertEqual('String[]', get_type('public String[] fizzBuzz(int start, int end) {}'))
+    def test_int_array(self):
+        self.assertEqual('int[]', get_type('public int[] fizzArray(int n) {}'))
     def test_bool_array(self):
-        pass
+        #Not from a bat
+        self.assertEqual('boolean[]', get_type('private static boolean[] boolMaster(boolean[] bools_for_fools) {}'))
+    def test_char_array(self):
+        #Not from a bat
+        self.assertEqual('char[]', get_type('protected final char[] charCarp(int a, int b) {}'))
+    def test_char(self):
+        #Not from a bat
+        self.assertEqual('char', get_type('public final char charCar(int a) {]'))
     def test_int(self):
-        pass
+        self.assertEqual('int', get_type('public int diff21(int n) {}'))
     def test_string(self):
-        pass
-    def test_tup(self):
-        pass
+        self.assertEqual('String', get_type('public String nonStart(String a, String b) {}'))
     def test_float(self):
-        pass
+        #Not from a bat
+        self.assertEqual('float', get_type('public float floaty(float a, float b) {}'))
 
 if __name__ == '__main__':
     unittest.main()
