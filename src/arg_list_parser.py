8509cbc4b8a42e9e1a93c07ab14929b9b0b697af
WorryingWonton <ahcampbellster@gmail.com>
2018-01-15 16:09:57 -0700

    Modified regex to match() + Revised Error Handling
    Changed regex statements to match() instead of search(), to eliminate the possibility of the statements returning something when their regex criteria are not fulfilled on the first character of s.

--- src/arg_list_parser.py
+++ src/arg_list_parser.py
@@ -3,31 +3,50 @@ import re
 import unittest
 
 def integer(s, index):
-    int_literal = re.search('-?([0-9]*)', s[index:]).group(1)
-    index += len(int_literal)
-    return (int_literal, index)
-
+    m = re.match('-?([0-9]+)', s[index:])
+    if m:
+        m = m.group(1)
+        index += len(m)
+        return (m, index)
+    else:
+        return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 def floating_point(s, index):
-    float_literal = re.search('-?([0-9]*)\.([0-9]*)', s[index:]).group(1)
-    index += len(float_literal)
-    return(float_literal, index)
+    m = re.match('-?([0-9]+)\.([0-9]*)', s[index:])
+    if m:
+        m = m.group(1)
+        index += len(m)
+        return (m, index)
+    else:
+        return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 def character(s, index):
-    char_literal = re.search('\'[^\']\'', s[index:]).group(1)
-    index += len(char_literal)
-    return (char_literal, index)
+    m = re.match('\'[^\']\'', s[index:])
+    if m:
+        m = m.group(1)
+        index += len(m)
+        return (m, index)
+    else:
+        return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 
 def string(s, index):
-    string_literal = re.search('"[^"]*"', s[index:]).group(1)
-    index += len(string_literal)
-    return (string_literal, index)
+    m = re.match('"[^"]*"', s[index:])
+    if m:
+        m = m.group(1)
+        index += len(m)
+        return (m, index)
+    else:
+        return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 def boolean(s, index):
-    bool_literal = re.search('(true|false)', s[index:]).group(1)
-    index += len(bool_literal)
-    return (bool_literal, index)
+    m = re.search('(true|false)', s[index:])
+    if m:
+        m = m.group(1)
+        index += len(m)
+        return (m, index)
+    else:
+        return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 
 def array(s, index):
@@ -35,12 +54,12 @@ def array(s, index):
 
 class TestLiteralFunctions(unittest.TestCase):
     def test_interger(self):
-        self.assertEqual(('', 0), integer('', 0))
+        self.assertEqual(('ERROR: Expecting digit, got ', 0), integer('', 0))
         self.assertEqual(('12345', 5), integer('12345', 0))
         self.assertEqual(('234', 4), integer('1234', 1))
         self.assertEqual(('1234', 4), integer('1234.5678', 0))
         self.assertEqual(('123', 3), integer('123a345.789', 0))
-        self.assertEqual(('', 0), integer('.1234', 0))
+        self.assertEqual(('ERROR: Expecting digit, got .', 0), integer('.1234', 0))
     def test_floating_point(self):
         pass
     def test_character(self):
