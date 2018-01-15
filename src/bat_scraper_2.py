80be9f3cc9174b1a3340b65d89322778c4d25bad
WorryingWonton <ahcampbellster@gmail.com>
2018-01-14 18:34:36 -0700

    Added functions to generate valid input code for the codingbat website, and a function to submit that code and generate a list of results.

--- src/bat_scraper_2.py
+++ src/bat_scraper_2.py
@@ -10,17 +10,52 @@ def get_type(text):
     my_reg = re.search('(public|private|protected) ((static|final) )?([0-9a-zA-Z<>\[\]]*) ', text)
     return my_reg.group(4)
 
-def generate_return(text, type):
-    generic_type_dict = {'int': 'a = 0; return a;', 'String': 'String a = "hello; return a;', 'String[]': 'String[] a = \{\"Hello\"\}; return a;',
-                         'int[]': 'int[] a = {0}; return a', 'boolean': 'a = true; return a', 'List<Integer>': 'List<Integer> a = new ArrayList<Integer>(); return a;',
-                         'List<String>': 'List<String> a = new ArrayList<String>(); return a;', 'char': '', 'char[]': '', 'boolean[]': '', 'float': '', 'float[]': ''}
+def generate_return(type):
+    generic_type_dict = {'int': 'a = 0; return a;',
+                         'String': 'String a = "hello"; return a;',
+                         'String[]': 'String[] a = {"Hello"}; return a;',
+                         'int[]': 'int[] a = {0}; return a;',
+                         'boolean': 'return true;',
+                         'List<Integer>': 'List<Integer> a = new ArrayList<Integer>(); return a;',
+                         'List<String>': 'List<String> a = new ArrayList<String>(); return a;',
+                         'char': 'char a = \'a\'; return a;',
+                         'char[]': 'char[] a = {\'a\'}; return a;',
+                         'boolean[]': 'boolean[] a = {true}; return a;',
+                         'float': 'a = 2.0; return a;',
+                         'float[]': 'float[] a = {2.0}; return a;'}
+    return generic_type_dict[type]
+
+def generate_code(text, return_statement):
+    return text[0:text.index('}')] + return_statement + '}'
+
+def submit_code(code, pid):
+    response = requests.post('http://codingbat.com/run', data={"id": pid, 'code':code})
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rows = soup.select('div table tr')
+    row_list = []
+    for row in list(rows)[1:]:
+        invocation_td = next(row.children)
+        row_list.append(invocation_td.text.strip())
+    return row_list[0:-1]
+
+def get_expected(row_list):
     pass
 
-# bat = get_bat('p159531')
-# print(bat)
-# print(get_type(bat))
+def get_invocation(row_list):
+    pass
 
-class Testget_type(unittest.TestCase):
+
+
+pid = 'p159531'
+bat = get_bat(pid)
+print(bat)
+print(get_type(bat))
+a = generate_code(bat, generate_return(get_type(bat)))
+print(a)
+b = submit_code(a, pid)
+print(b)
+
+class Test_get_type(unittest.TestCase):
     def test_bool(self):
         self.assertEqual('boolean', get_type('public boolean lucky13(int[] nums) {}'))
     def test_list_int(self):
@@ -48,6 +83,35 @@ class Testget_type(unittest.TestCase):
         #Not from a bat
         self.assertEqual('float', get_type('public float floaty(float a, float b) {}'))
 
+class Test_generate_return(unittest.TestCase):
+    def test_int(self):
+        self.assertEqual('a = 0; return a;', generate_return('int'))
+    def test_String(self):
+        self.assertEqual('String a = "hello"; return a;', generate_return('String'))
+    def test_char(self):
+        self.assertEqual('char a = \'a\'; return a;', generate_return('char'))
+    def test_bool(self):
+        self.assertEqual('return true;', generate_return('boolean'))
+    def test_float(self):
+        self.assertEqual('a = 2.0; return a;', generate_return('float'))
+    def test_List_int(self):
+        pass
+    def test_List_String(self):
+        pass
+    def test_array_int(self):
+        pass
+    def test_array_String(self):
+        pass
+    def test_array_char(self):
+        pass
+    def test_array_bool(self):
+        self.assertEqual('boolean[] a = {true}; return a;', generate_return('boolean[]'))
+    def test_array_float(self):
+        pass
+
+class Test_generate_code(unittest.TestCase):
+    def test_cigarParty(self):
+        self.assertEqual('public boolean cigarParty(int cigars, boolean isWeekend) {a = true; return a;}', generate_code('public boolean cigarParty(int cigars, boolean isWeekend) {}', 'a = true; return a;'))
 if __name__ == '__main__':
     unittest.main()
 
