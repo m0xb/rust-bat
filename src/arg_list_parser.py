b92b28441854322ae11c2ba581ddccb794403639
WorryingWonton <ahcampbellster@gmail.com>
2018-01-15 17:03:07 -0700

    Added unittests for floating_point
    Added unittests for floating_point, and consolidated the caputure groups for floating_points regex.  Also, m now accesses group 0 instead of group 1 for all functions.

--- src/arg_list_parser.py
+++ src/arg_list_parser.py
@@ -5,15 +5,15 @@ import unittest
 def integer(s, index):
     m = re.match('-?([0-9]+)', s[index:])
     if m:
-        token = m.group(1)
+        token = m.group(0)
         return (token, index + len(token))
     else:
         return (f'ERROR: Expecting digit, got {s[index:index+1]}', index)
 
 def floating_point(s, index):
-    m = re.match('-?([0-9]+)\.([0-9]*)', s[index:])
+    m = re.match('-?([0-9]+\.[0-9]*)', s[index:])
     if m:
-        token = m.group(1)
+        token = m.group(0)
         return (token, index + len(token))
     else:
         return (f'ERROR: Expecting float, got {s[index:index+1]}', index)
@@ -21,7 +21,7 @@ def floating_point(s, index):
 def character(s, index):
     m = re.match('\'[^\']\'', s[index:])
     if m:
-        token = m.group(1)
+        token = m.group(0)
         return (token, index + len(token))
     else:
         return (f'ERROR: Expecting char, got {s[index:index+1]}', index)
@@ -30,7 +30,7 @@ def character(s, index):
 def string(s, index):
     m = re.match('"[^"]*"', s[index:])
     if m:
-        token = m.group(1)
+        token = m.group(0)
         return (token, index + len(token))
     else:
         return (f'ERROR: Expecting String, got {s[index:index+1]}', index)
@@ -38,7 +38,7 @@ def string(s, index):
 def boolean(s, index):
     m = re.search('(true|false)', s[index:])
     if m:
-        token = m.group(1)
+        token = m.group(0)
         return (token, index + len(token))
     else:
         return (f'ERROR: Expecting bool, got {s[index:index+1]}', index)
@@ -56,7 +56,17 @@ class TestLiteralFunctions(unittest.TestCase):
         self.assertEqual(('123', 3), integer('123a345.789', 0))
         self.assertEqual(('ERROR: Expecting digit, got .', 0), integer('.1234', 0))
     def test_floating_point(self):
-        pass
+        self.assertEqual(('ERROR: Expecting float, got ', 0), floating_point('', 0))
+        self.assertEqual(('ERROR: Expecting float, got 1', 0), floating_point('1', 0))
+        self.assertEqual(('ERROR: Expecting float, got ', 4), floating_point('', 4))
+        self.assertEqual(('ERROR: Expecting float, got ', 4), floating_point('1.0', 4))
+        self.assertEqual(('ERROR: Expecting float, got .', 0), floating_point('.234', 0))
+        self.assertEqual(('1.', 2), floating_point('1.', 0))
+        self.assertEqual(('1.', 2), floating_point('1..0', 0))
+        self.assertEqual(('1.1', 3), floating_point('1.1', 0))
+        self.assertEqual(('1234.0', 6), floating_point('1234.0', 0))
+        self.assertEqual(('1234.56789', 10), floating_point('1234.56789', 0))
+        self.assertEqual(('234.56789', 10), floating_point('1234.56789', 1))
     def test_character(self):
         pass
     def test_string(self):
